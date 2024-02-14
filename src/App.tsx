cad81a2d0694879ffc4808a47551869420f27d05
Sam.Markowitz <sam.markowitz@sisense.com>
2024-02-14 11:49:44 +0200

    add chart data

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,46 @@ import * as DM from './models/sample-retail';
 
 function App() {
   return (
-    <Chart
-      dataSet={DM.DataSource}
-      chartType={'column'}
-      dataOptions={{
-        category: [DM.DimProducts.CategoryName],
-        value: [measureFactory.sum(DM.Fact_Sale_orders.OrderRevenue)],
-      }}
-      styleOptions={{
-        width: 1000,
-        height: 400,
-      }}
-    />
+    <>
+      <Chart
+        dataSet={DM.DataSource}
+        chartType={'column'}
+        dataOptions={{
+          category: [DM.DimProducts.CategoryName],
+          value: [
+            measureFactory.sum(
+              DM.Fact_Sale_orders.OrderRevenue,
+              'Total Revenue'
+            ),
+            {
+              column: measureFactory.sum(
+                DM.Fact_Sale_orders.OrderQty,
+                'Total Order Quantity'
+              ),
+              showOnRightAxis: true,
+            },
+          ],
+        }}
+        styleOptions={{
+          width: 1000,
+          height: 400,
+          yAxis: { title: { enabled: true, text: 'Revenue' } },
+          y2Axis: { title: { enabled: true, text: 'Quantity' } },
+        }}
+      />
+      <Chart
+        dataSet={DM.DataSource}
+        chartType={'column'}
+        dataOptions={{
+          category: [DM.DimProducts.CategoryName],
+          value: [measureFactory.sum(DM.Fact_Sale_orders.OrderRevenue)],
+        }}
+        styleOptions={{
+          width: 1000,
+          height: 400,
+        }}
+      />
+    </>
   );
 }
 
