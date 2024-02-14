be4bca7d8300e00c22a959fd712138e56a32fd7e
Sam.Markowitz <sam.markowitz@sisense.com>
2024-02-14 10:12:08 +0200

    add first chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Chart } from '@sisense/sdk-ui';
+import { measureFactory } from '@sisense/sdk-data';
+import * as DM from './models/sample-retail';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Chart
+      dataSet={DM.DataSource}
+      chartType={'column'}
+      dataOptions={{
+        category: [DM.DimProducts.CategoryName],
+        value: [measureFactory.sum(DM.Fact_Sale_orders.OrderRevenue)],
+      }}
+      styleOptions={{
+        width: 1000,
+        height: 400,
+      }}
+    />
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { SisenseContextProvider } from '@sisense/sdk-ui';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SisenseContextProvider
+      url={import.meta.env.VITE_APP_SISENSE_URL}
+      token={import.meta.env.VITE_APP_SISENSE_TOKEN}
+    >
+      <App />
+    </SisenseContextProvider>
+  </React.StrictMode>
+);
